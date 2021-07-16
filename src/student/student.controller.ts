442393f6e9b7ff5cc1adbba9511dd73a6bd85f60
Nguyen Duc Duy <duynd1@tinhvan.com>
2021-07-16 19:37:47 +0700

    elastic search

--- src/student/student.controller.ts
+++ src/student/student.controller.ts
@@ -1,12 +1,24 @@
 import { Controller, Get, Req } from '@nestjs/common';
 import { Request } from 'express';
+import { StudentService } from './student.service';
 
 @Controller('student')
 export class StudentController {
+  constructor(private studentService: StudentService) {}
+
   @Get('list')
-  listStudents(@Req() request: Request) {
-    const { school, clazz, dob, name } = request.query;
+  async listStudents(@Req() request: Request) {
+    const { school, dob, name, class: clazz } = request.query;
+
+    const result = await this.studentService.findStudent(
+      school as string,
+      clazz as string,
+      dob as string,
+      name as string,
+    );
 
 
+
+    return result;
   }
 }
--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { ElasticsearchService } from '@nestjs/elasticsearch';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+  constructor(private es: ElasticsearchService) {}
+
+  async findStudent(school: string, clazz: string, dob: string, name: string) {
+    const buildAndQuery = () => {
+      const query = [];
+
+      const singularMatchCondition = (key: string, value: string) => ({
+        match: {
+          [key]: {
+            query: value,
+            fuzziness: 2,
+            analyzer: 'vi_analyzer',
+          },
+        },
+      });
+
+      const dateCondition = (key: string, value: string) => ({
+        range: {
+          [key]: {
+            gte: value,
+            lte: value,
+          },
+        },
+      });
+
+      if (school) query.push(singularMatchCondition('school', school));
+      if (clazz) query.push(singularMatchCondition('class', clazz));
+      if (dob) query.push(dateCondition('dob', dob));
+      if (name) query.push(singularMatchCondition('name', name));
+
+      return query;
+    };
+
+    try {
+      const result = await this.es.search({
+        index: 'student',
+        body: {
+          query: {
+            bool: {
+              must: buildAndQuery(),
+            },
+          },
+        },
+      });
+
+      const maxScore = result.body.hits.max_score;
+
+      return result.body?.hits?.hits
+        ?.filter((doc) => doc._score >= maxScore * 0.9)
+        .map((doc) => ({
+          id: doc._source.index,
+          name: doc._source.name,
+          class: doc._source.class,
+          dob: doc._source.dob,
+          school: doc._source.school,
+        }));
+    } catch (e) {
+      if (e.message.includes('[parse_exception]'))
+        throw new HttpException('dob format must be yyyy-MM-dd', 400);
+    }
+  }
+}
